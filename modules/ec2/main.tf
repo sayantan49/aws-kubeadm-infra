1b89fec80ac516133a597aa2d30ffd064ff4d860
sayantan chowdhury <sayantan.chowdhury3@gmail.com>
2025-03-03 00:30:23 +0530

    refactored

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0912084d6d35eb634" # Change this to a valid AMI
-  instance_type = "t2.micro"
+resource "aws_instance" "" {
+  ami           = "ami-09a9858973b288bdd" # Change this to a valid AMI
+  instance_type = "t3.medium"
   vpc_security_group_ids = [var.security_group_id]
 
   depends_on = [var.security_group_id]  # Ensures Security Group is created first
