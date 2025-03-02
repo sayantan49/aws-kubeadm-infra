8fbba95303d13bd5b8663c8d64b833671d326b83
sayantan chowdhury <sayantan.chowdhury3@gmail.com>
2025-03-02 20:02:43 +0530

    multiple changes

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,3 +3,9 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "vpc_id" {
+  description = "VPC ID (Will be created if not provided)"
+  type        = string
+  default     = ""  # Ensures Terraform does NOT prompt for input
+}
