a1b5bd30d845fd248d8af5bd168314dc258db581
sayantan chowdhury <sayantan.chowdhury3@gmail.com>
2025-03-02 17:57:48 +0530

    changes in dev and ec2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -26,6 +26,7 @@ module "vpc" {
 module "security_groups" {
   source = "../../modules/security_groups"
   vpc_id = module.vpc.vpc_id
+  depends_on = [module.vpc]
 }
 
 module "ec2" {
@@ -33,4 +34,5 @@ module "ec2" {
   vpc_id       = module.vpc.vpc_id
   master_sg_id = module.security_groups.master_sg_id
   worker_sg_id = module.security_groups.worker_sg_id
+  depends_on = [module.vpc, module.security_groups]
 }
